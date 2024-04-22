198aa71c57fcb014231cc4ae8e52d4f184a28c76
Gortych <damian.gortych@accenture.com>
2024-04-22 12:32:18 +0200

    M

--- CliftonApp0_1.py
+++ CliftonApp0_1.py
@@ -1,41 +1,9 @@
 import streamlit as st
 import pandas as pd
 import os
-from zipfile import ZipFile
-
-import CliftonPlots
 import CliftonCorresps
-
 import json
-import openai
 
-# Load configuration from JSON file
-with open("config.json", mode="r") as f:
-    config = json.load(f)
-
-client = openai.AzureOpenAI(
-        azure_endpoint=config["AZURE_ENDPOINT"],
-        api_key= config["AZURE_API_KEY"],
-        api_version="2023-12-01-preview")
-
-# Function to process the uploaded Excel file and generate result files
-def process_excel(input_file):
-    # Load the Excel file
-    
-    CliftonCorresps.create_excel(input_file)
-
-    # Save the processed Excel file
-    output_excel_path = "test.xlsx"
-    
-    png_file_paths = CliftonPlots.create_plots()
-    
-    return output_excel_path, png_file_paths
-
-def zip_files(file_paths, zip_name):
-    with ZipFile(zip_name, 'w') as zip:
-        for file in file_paths:
-            zip.write(file)
-            
 # Main function to run the Streamlit app
 def main():
     st.title("Clifton Strenghts Application")
@@ -45,18 +13,17 @@ def main():
 
     if uploaded_file is not None:
         # Process the Excel file
-        output_excel_path, png_file_paths = process_excel(uploaded_file)
-
-        zip_name = "result_files.zip"
-        zip_files([output_excel_path] + png_file_paths, zip_name)
-
-        with open("result_files.zip", "rb") as fp:
-            btn = st.download_button(
-                label="Download Results",
-                data=fp,
-                file_name="results.zip",
-                mime="application/zip"
-    )
+        output_excel_path = CliftonCorresps.create_excel(uploaded_file)
+        st.markdown("### Download Output Excel File")
+        st.write("Click the button below to download the output Excel file and upload it to provided SharePoint location:")
+        with open(output_excel_path, "rb") as file:
+            contents = file.read()
+        st.download_button(label="Download", data=contents, file_name="FactsTeam.xlsx", mime="application/octet-stream")
+        st.markdown("[SharePoint Location](https://ts.accenture.com/sites/CliftonApp/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2FCliftonApp%2FShared%20Documents%2FCliftonApp&viewid=b471a66b%2D616f%2D4bd1%2Db10a%2D804dfeaa698c)")
+        
+        st.markdown("### Power BI Report")
+        
+        st.markdown('<iframe title="CliftonApp.v.4.2" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=c484c675-0935-46fb-a1fa-613daea31592&autoAuth=true&ctid=e0793d39-0939-496d-b129-198edd916feb" frameborder="0" allowFullScreen="true"></iframe>',unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
--- CliftonCorresps.py
+++ CliftonCorresps.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 from scipy.stats import chi2_contingency
 
-file_name = "IPT_CliftonInputs.xlsx"
 sheet_name = "Sheet1"
+output_file_path = "FactsTeam.xlsx"
 def load_data(file_path, sheet_name):
     # Load data from Excel file
     df = pd.read_excel(file_path, sheet_name=sheet_name, header=0, index_col=0)
@@ -28,7 +28,7 @@ def calculate_pearson_residuals(df_numeric):
         print("Error:", e)
         return None
 
-def correspondence_analysis(df_numeric, normalization='symmetric', top_strengths=True):
+def correspondence_analysis(df_numeric, normalization='row_principal', top_strengths=True):
     if top_strengths:
         df_numeric.iloc[:, 0:13] = df_numeric.iloc[:, 0:13].fillna(24)  # Fill NaN with middle value (24)
     # Convert DataFrame to numpy array
@@ -53,7 +53,6 @@ def correspondence_analysis(df_numeric, normalization='symmetric', top_strengths
     
     # SVD of Pearson residuals
     U, s, Vt = np.linalg.svd(residuals, full_matrices=False)
-    explained_variance = np.square(s) / (total - 1)
     
     # Correspondence analysis scores for rows and columns
     row_ca_scores = U[:, :2] * np.sqrt(total - 1)
@@ -63,67 +62,26 @@ def correspondence_analysis(df_numeric, normalization='symmetric', top_strengths
     row_ca_df = pd.DataFrame(row_ca_scores, index=df_numeric.index, columns=['X_dimension', 'Y_dimension'])
     col_ca_df = pd.DataFrame(col_ca_scores, index=df_numeric.columns, columns=['X_dimension', 'Y_dimension'])
 
-    return row_ca_df, col_ca_df, explained_variance
+    return row_ca_df, col_ca_df
 
-def save_data(row_ca_df, col_ca_df, explained_variance):
+def save_data(row_ca_df, col_ca_df):
     # Save outputs to Excel workbook
-    output_file_path =  "test.xlsx"
-    print(output_file_path)
     with pd.ExcelWriter(output_file_path) as writer:
-        row_ca_df.to_excel(writer, sheet_name='Row_CA_Scores')
-        col_ca_df.to_excel(writer, sheet_name='Column_CA_Scores')
-        pd.DataFrame(explained_variance, columns=['Explained_Variance']).to_excel(writer, sheet_name='Explained_Variance')
+        row_ca_df.to_excel(writer, sheet_name='FactsTeamCoordinates')
+        col_ca_df.to_excel(writer, sheet_name='FactsTeamStrenghtCoordinates')
 
 def create_excel(file):
     df = load_data(file, sheet_name)
 
     # Preprocess the data
-    
     df_numeric = preprocess_data(df)
 
     # Calculate Pearson residuals
-    pearson_residuals = calculate_pearson_residuals(df_numeric)
    
     # Perform correspondence analysis
-    normalization_choice = "symmetric"
+    normalization_choice = 'row_principal'
     top_strengths = "yes"
 
-    row_ca_df, col_ca_df, explained_variance = correspondence_analysis(df_numeric, normalization=normalization_choice, top_strengths=top_strengths)
-    
-    save_data(row_ca_df, col_ca_df, explained_variance)
-
-
-
-
-""""
-def main():
-    df = load_data(file_name, sheet_name)
-
-    # Preprocess the data
-    
-    df_numeric = preprocess_data(df)
-
-    # Calculate Pearson residuals
-    pearson_residuals = calculate_pearson_residuals(df_numeric)
-    print("Pearson Residuals:")
-    print(pearson_residuals)
-
-    # Perform correspondence analysis
-    normalization_choice = input("Choose normalization (symmetric, row_principal, column_principal): ")
-    top_strengths = input("Focus only on top 13 strengths? (yes/no): ").lower() == 'yes'
-
-    row_ca_df, col_ca_df, explained_variance = correspondence_analysis(df_numeric, normalization=normalization_choice, top_strengths=top_strengths)
-    print("\nCorrespondence Analysis Scores:")
-    print("\nPersons:")
-    print(row_ca_df)
-    print("\nStrengths:")
-    print(col_ca_df)
-    print("\nExplained Variance:")
-    print("X dimension:", explained_variance[0])
-    print("Y dimension:", explained_variance[1])
-
-    save_data(row_ca_df, col_ca_df, explained_variance)
-
-if __name__ == "__main__":
-    main()
-"""
+    row_ca_df, col_ca_df = correspondence_analysis(df_numeric, normalization=normalization_choice, top_strengths=top_strengths)
+    save_data(row_ca_df, col_ca_df)
+    return output_file_path
